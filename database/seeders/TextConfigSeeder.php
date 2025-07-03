117c0b8be84b031a94a806ac59d0d50029800d30
fuadTamim1 <fuad89573@gmail.com>
2025-07-04 02:24:46 +0300

    railway dependes

--- database/seeders/TextConfigSeeder.php
+++ database/seeders/TextConfigSeeder.php
@@ -13,14 +13,17 @@ class TextConfigSeeder extends Seeder
     public function run(): void
     {
         $textConfigs = [
-            // Example text configs
-            ['key' => 'about_us_text', 'value' => 'We are the IEEE CS BAU Chapter, empowering future engineers.'],
-            ['key' => 'homepage_banner_text', 'value' => 'Welcome to the IEEE CS BAU Chapter!'],
-            ['key' => 'contact_page_info', 'value' => 'Contact us at cs@ieee.bau.edu.jo for support.'],
-            ['key' => 'footer_disclaimer', 'value' => 'All rights reserved. IEEE CS BAU 2025.'],
-            ['key' => 'privacy_policy_text', 'value' => 'Your privacy is important to us.'],
-            ['key' => 'terms_of_service_text', 'value' => 'Please read our terms of service carefully.'],
-            // Add more as needed
+            ['key' => 'home_hero_title', 'value' => 'Welcome to Our Website'],
+            ['key' => 'home_hero_subtitle', 'value' => 'Discover amazing content and services'],
+            ['key' => 'about_page_intro', 'value' => 'Learn more about our company and missions'],
+            ['key' => 'contact_page_info', 'value' => 'Get in touch with our team'],
+            ['key' => 'footer_copyright', 'value' => '© 2025 Your Company. All rights reserved.'],
+            ['key' => 'facebook-link', 'value' => 'https://web.facebook.com/groups/232534844172880'],
+            ['key' => 'instagram-link', 'value' => 'https://www.instagram.com/ieee_bau_cs/'],
+            ['key' => 'linkedin-link', 'value' => 'https://www.linkedin.com/company/ieee-bau-cs-computer-society/'],
+            ['key' => 'location', 'value' => 'Al Balqa Applied University Engineering Technology College, Al Hizam Al Daeri St 527, Amman'],
+            ['key' => 'email', 'value' => 'ieeecsbau@ieee.org'],
+            ['key' => 'google_map_locaction', 'value' => 'https://maps.app.goo.gl/V5Lx9KGiZb12265F7'],
         ];
 
         foreach ($textConfigs as $config) {
