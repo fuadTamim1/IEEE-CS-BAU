da7a0958a7f2011c35b2fd1a832da5e30834f656
fuadTamim1 <fuad89573@gmail.com>
2025-07-04 03:22:05 +0300

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Make sure this file has executable permissions, run `chmod +x build-app.sh`
+# Make sure this file has executable permissions, run ` `
 
 # Exit the script if any command fails
-set -e
+# set -e
 
 # Build assets using NPM
 npm run build
