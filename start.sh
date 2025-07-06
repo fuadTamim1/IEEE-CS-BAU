71dd866d90c49e6622b2a3463191c96138a4e324
fuadTamim1 <fuad89573@gmail.com>
2025-07-06 08:44:52 +0300

    updates

--- start.sh
+++ start.sh
@@ -4,3 +4,8 @@ php artisan migrate --force
 php artisan db:seed
 php artisan storage:link
 php artisan filament:optimize
+
+#!/bin/bash
+# Make sure this file has executable permissions, run `chmod +x start.sh`
+# Run migrations, set up nginx conf and run nginx
+node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
